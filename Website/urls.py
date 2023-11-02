51e1e8a07d0bc09cef77e6ae2097296df659ea83
Szaneron <szaneron@gmail.com>
2023-11-02 19:50:56 +0100

    Created recipes view template. Working with api request.

--- Website/urls.py
+++ Website/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("login/", views.login_user, name="login_user"),
     path("logout/", views.logout_user, name="logout_user"),
     path("home/", views.home, name="home"),
+    path("recipe/", views.recipe, name="recipe"),
     path("recipe/detail/<int:recipe_id>/", views.recipe_detail, name="recipe_detail"),
 ]
--- Website/views.py
+++ Website/views.py
@@ -3,6 +3,7 @@ import os
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.utils.safestring import mark_safe
 from dotenv import load_dotenv
@@ -88,18 +89,58 @@ def logout_user(request):
 def home(request):
     # Get API key environment variable
     api_key = os.getenv('SPOONACULAR_API_KEY')
+
     popular_url = 'https://api.spoonacular.com/recipes/informationBulk?ids=638248,647634,639411,1055614&includeNutrition=true&apiKey=' + api_key
     latest_url = 'https://api.spoonacular.com/recipes/informationBulk?ids=654125,656752,800754,1098357&includeNutrition=true&apiKey=' + api_key
-    response1 = requests.get(popular_url)
-    response2 = requests.get(latest_url)
+    response_popular = requests.get(popular_url)
+    response_latest = requests.get(latest_url)
 
-    if response1.status_code == 200 and response2.status_code == 200:
-        popular_recipes = response1.json()
-        latest_recipes = response2.json()
+    if response_popular.status_code == 200 and response_latest.status_code == 200:
+        popular_recipes = response_popular.json()
+        latest_recipes = response_latest.json()
         return render(request, 'home_page.html',
                       {'popular_recipes': popular_recipes, 'latest_recipes': latest_recipes, })
     else:
         return render(request, 'home_page.html')
+
+
+def recipe(request):
+    # Get API key environment variable
+    api_key = os.getenv('SPOONACULAR_API_KEY')
+
+    random_recieps_url = 'https://api.spoonacular.com/recipes/random?number=12&apiKey=' + api_key
+    response_random_recieps = requests.get(random_recieps_url)
+    random_recieps = response_random_recieps.json()
+    if random_recieps['code'] == 402:
+        print(response_random_recieps)
+
+        return render(request, 'recipe.html')
+    elif random_recieps.status_code == 200:
+        # Get id's from random to put them into another request with nutrition
+        random_ids = []
+        for recipe in random_recieps['recipes']:
+            random_ids.append(recipe['id'])
+        ids_string = ','.join(map(str, random_ids))
+        recipes_url = f'https://api.spoonacular.com/recipes/informationBulk?ids={ids_string}&includeNutrition=true&apiKey=' + api_key
+        response_recieps = requests.get(recipes_url)
+
+        if response_recieps.status_code == 200:
+            recipes = response_recieps.json()
+            print(recipes)
+
+            paginator = Paginator(recipes, 5)  # Dzieli listę na strony z po 5 rekordami na każdej stronie
+            page_number = request.GET.get('page')
+            paged_recipes = paginator.get_page(page_number)
+
+            context = {
+                'recipes': recipes,
+                'paged_recipes': paged_recipes,
+            }
+            return render(request, 'recipe.html', context)
+        else:
+            return render(request, 'recipe.html')
+    else:
+        return render(request, 'recipe.html')
 
 
 def recipe_detail(request, recipe_id):
